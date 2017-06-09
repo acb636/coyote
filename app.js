198bb5aa837cb3e75087f9a35d9b963a095b18bb
Flávio Mendes <flaviojmendes@gmail.com>
2017-06-09 07:46:53 -0300

    adicionado botão copy all

--- app.js
+++ app.js
@@ -58,6 +58,15 @@
       return getValuesForKey($scope.objJson, $scope.formData.selectedKey);
     };
 
+    $scope.formatArray = function (ary) {
+
+      var formattedText = '';
+      angular.forEach(ary, function(item){
+        formattedText = formattedText + item + '\n';
+      });
+      return formattedText;
+    }
+
     $scope.updateToggle = function () {
       $("#copyToggle").off('click');
       $("#copyToggle").on( "click", function() {
@@ -86,5 +95,6 @@
 
 $(document).ready(function() {
   var clipboard = new Clipboard('.clipboard');
+  var clipboard = new Clipboard('.clipboardAll');
 
 });
